f009985aff5898c27522b9b1723a690a1c996f1c
crew7 <tyjesp@gmail.com>
2023-06-07 20:28:24 -0400

    Added multi-string support and made strings case insensitive

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,25 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, dictionary)
+def substrings(words, dictionary)
+    words = words.downcase
+    dictionary = downcase_array(dictionary)
+    #Downcases to make case insensitive
+
+    words = words.split(" ")
+
     substr = dictionary.reduce( Hash.new(0) ) do |accumulator, dictionaryEntry|
-        if word.include?(dictionaryEntry)
-            accumulator[dictionaryEntry] += 1
-            accumulator
-        else
-            accumulator
+        words.each do |word|
+            if word.include?(dictionaryEntry)
+                accumulator[dictionaryEntry] += 1
+            end
         end
+        accumulator
     end
     substr
 end
 
-substrings("below",dictionary)
+def downcase_array(arr)
+    downcased = arr.map {|word| word.downcase}
+end
+
+puts substrings("Howdy partner, sit down! How's it going?",dictionary)
